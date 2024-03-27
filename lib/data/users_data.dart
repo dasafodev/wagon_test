dcc0e054bb6a246783f3d6850a71ef8553d50927
Santiago Forero A <sanfo0620@gmail.com>
2024-03-27 16:14:52 -0500

    feat: Add users_data.dart and vehicle_data.dart files

---
+++ lib/data/users_data.dart
@@ -0,0 +1,7 @@
+import 'package:wagon/enums/role.dart';
+import 'package:wagon/models/user.dart';
+
+Map<String, User> usersData = {
+  "1": User(id: '1', role: Role.driver, name: 'John Doe'),
+  '2': User(id: '2', role: Role.driver, name: 'Jane Doe')
+};
---
+++ lib/data/vehicle_data.dart
@@ -0,0 +1,6 @@
+import 'package:wagon/models/vehicle.dart';
+
+Map<String, Vehicle> vehiclesData = {
+  "1": Vehicle(id: '1', plate: 'ABC123'),
+  '2': Vehicle(id: '2', plate: 'XYZ987')
+};
